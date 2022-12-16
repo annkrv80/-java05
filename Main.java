ca7577b775f8b05a755a82d79eb8ca7b16a30c60
Anna <annkrv80@yandex.ru>
2022-12-16 20:14:55 +0700

    Файл Мейн

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+   
+    public static void main(String[] args) {
+        
+        /*FirstProblem firstProblem = new FirstProblem();
+        Map<Integer, String> passports = firstProblem.fillMap(new HashMap<>());
+        Scanner in = new Scanner(System.in);
+        firstProblem.getPassportInfoByName(passports, in.next());
+        in.close();*/
+
+
+
+        /*SecondProblem secondProblem = new SecondProblem();
+        String a="foo";
+        String b ="bar";
+        System.out.print(secondProblem.isIsomorphic(a, b));*/
+
+        ThirdProblem thirdProblem = new ThirdProblem();
+        thirdProblem.sortedMap("Мороз и солнце день чудесный Еще ты дремлешь " +
+                "друг прелестный Пора красавица проснись");
+        
+
+        
+    }
+
+ 
+        
+}
+       
+
+
+
+
+
+
+
+
